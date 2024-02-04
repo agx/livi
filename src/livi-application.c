agx/livi

commit 7fdcdb8efc888f0e4e8d84dc0424df0f447cfbd1

Add LiviApplication
This allows us to carry application state

src/livi-application.c
@@ -1,0 +1,47 @@
+/*
+ * Copyright (C) 2023 Guido Günther <agx@sigxcpu.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Guido Günther <agx@sigxcpu.org>
+ */
+
+#define G_LOG_DOMAIN "livi-application"
+
+#include "livi-config.h"
+
+#include "livi-application.h"
+
+struct _LiviApplication {
+  AdwApplication parent;
+};
+G_DEFINE_TYPE (LiviApplication, livi_application, ADW_TYPE_APPLICATION)
+
+
+static void
+livi_application_dispose (GObject *object)
+{
+  G_OBJECT_CLASS (livi_application_parent_class)->dispose (object);
+}
+
+
+static void
+livi_application_class_init (LiviApplicationClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->dispose = livi_application_dispose;
+}
+
+
+static void
+livi_application_init (LiviApplication *self)
+{
+}
+
+
+LiviApplication *
+livi_application_new (void)
+{
+  return g_object_new (LIVI_TYPE_APPLICATION, NULL);
+}

src/main.c
@@ -10,6 +10,7 @@
 #define G_LOG_DOMAIN "livi-main"
 
 #include "livi-config.h"
+#include "livi-application.h"
 #include "livi-url-processor.h"
 #include "livi-window.h"
 
@@ -273,7 +274,7 @@ fix_broken_cache (void)
 int
 main (int argc, char *argv[])
 {
-  g_autoptr (AdwApplication) app = NULL;
+  g_autoptr (LiviApplication) app = NULL;
   const GOptionEntry options[] = {
     { "h264-demo", 0, 0, G_OPTION_ARG_NONE, NULL, "Play h264 demo", NULL },
     { "vp8-demo", 0, 0, G_OPTION_ARG_NONE, NULL, "Play VP8 demo", NULL },
@@ -297,7 +298,7 @@ main (int argc, char *argv[])
     return 1;
 
   ctx.url_processor = livi_url_processor_new ();
-  app = g_object_new (ADW_TYPE_APPLICATION,
+  app = g_object_new (LIVI_TYPE_APPLICATION,
                       "application-id", APP_ID,
                       "flags", G_APPLICATION_HANDLES_COMMAND_LINE,
                       "register-session", TRUE,

src/livi-application.h
@@ -1,0 +1,19 @@
+/*
+ * Copyright (C) 2023 Guido Günther <agx@sigxcpu.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <adwaita.h>
+
+G_BEGIN_DECLS
+
+#define LIVI_TYPE_APPLICATION (livi_application_get_type ())
+
+G_DECLARE_FINAL_TYPE (LiviApplication, livi_application, LIVI, APPLICATION, AdwApplication)
+
+LiviApplication *livi_application_new (void);
+
+G_END_DECLS

src/meson.build
@@ -1,5 +1,6 @@
 livi_sources = [
   'main.c',
+  'livi-application.c',
   'livi-window.c',
   'livi-gst-paintable.c',
   'livi-gst-sink.c',

commit b7894cd8dd51412996fc37a060bf65b522b87857

application: Move option handling into class
No need to have this split between two files

Fixes: 7fdcdb8 ("Add LiviApplication")

src/livi-application.c
@@ -354,9 +354,19 @@ livi_application_class_init (LiviApplicationClass *klass)
 }
 
 
+const GOptionEntry options[] = {
+  { "h264-demo", 0, 0, G_OPTION_ARG_NONE, NULL, "Play h264 demo", NULL },
+  { "vp8-demo", 0, 0, G_OPTION_ARG_NONE, NULL, "Play VP8 demo", NULL },
+  { "yt-dlp", 'Y', 0, G_OPTION_ARG_NONE, NULL, "Let yt-dlp process the URL", NULL },
+  { G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_STRING_ARRAY, NULL, NULL, "[FILE]" },
+  { NULL,}
+};
+
 static void
 livi_application_init (LiviApplication *self)
 {
+  g_application_add_main_option_entries (G_APPLICATION (self), options);
+
   self->url_processor = livi_url_processor_new ();
   self->mpris = livi_mpris_new ();
 
@@ -367,5 +377,9 @@ livi_application_init (LiviApplication *self)
 LiviApplication *
 livi_application_new (void)
 {
-  return g_object_new (LIVI_TYPE_APPLICATION, NULL);
+  return g_object_new (LIVI_TYPE_APPLICATION,
+                      "application-id", APP_ID,
+                      "flags", G_APPLICATION_HANDLES_COMMAND_LINE,
+                      "register-session", TRUE,
+                       NULL);
 }

src/main.c
@@ -90,13 +90,6 @@ int
 main (int argc, char *argv[])
 {
   g_autoptr (LiviApplication) app = NULL;
-  const GOptionEntry options[] = {
-    { "h264-demo", 0, 0, G_OPTION_ARG_NONE, NULL, "Play h264 demo", NULL },
-    { "vp8-demo", 0, 0, G_OPTION_ARG_NONE, NULL, "Play VP8 demo", NULL },
-    { "yt-dlp", 'Y', 0, G_OPTION_ARG_NONE, NULL, "Let yt-dlp process the URL", NULL },
-    { G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_STRING_ARRAY, NULL, NULL, "[FILE]" },
-    { NULL,}
-  };
 
   /* TODO: Until we configure the full pipeline */
   g_setenv ("GST_PLAY_USE_PLAYBIN3", "1", TRUE);
@@ -111,13 +104,7 @@ main (int argc, char *argv[])
   if (!fix_broken_cache ())
     return 1;
 
-  app = g_object_new (LIVI_TYPE_APPLICATION,
-                      "application-id", APP_ID,
-                      "flags", G_APPLICATION_HANDLES_COMMAND_LINE,
-                      "register-session", TRUE,
-                      NULL);
-
-  g_application_add_main_option_entries (G_APPLICATION (app), options);
+  app = livi_application_new ();
 
   g_signal_connect (app, "notify::screensaver-active", G_CALLBACK (on_screensaver_active_changed), NULL);
   g_unix_signal_add (SIGINT, on_shutdown_signal, app);