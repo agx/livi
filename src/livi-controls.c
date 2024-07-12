946dd1da75e8be75158feed92be25d73beb243c4
Guido Günther <agx@sigxcpu.org>
2024-07-12 07:37:44 +0200

    controls: Improve formating
    We want hh:mm:ss rather than just mm:ss.

--- src/livi-controls.c
+++ src/livi-controls.c
@@ -129,21 +129,43 @@ livi_controls_get_property (GObject    *object,
 }
 
 
+/* Verbatim from libcall-ui */
+static char *
+cui_call_format_duration (double duration)
+{
+#define MINUTE 60
+#define HOUR   (60 * MINUTE)
+  guint seconds, minutes;
+  GString *str = g_string_new ("");
+
+  if (duration > HOUR) {
+    int hours = (int) (duration / HOUR);
+    g_string_append_printf (str, "%u:", hours);
+    duration -= (hours * HOUR);
+  }
+
+  minutes = (int) (duration / MINUTE);
+  seconds = duration - (minutes * MINUTE);
+  g_string_append_printf (str, "%02u:%02u", minutes, seconds);
+
+  return g_string_free (str, FALSE);
+#undef HOUR
+#undef MINUTE
+}
+
+
 static void
 update_slider (LiviControls *self, GstClockTime value)
 {
-  g_autofree char *text = NULL;
-  guint64 pos, dur;
+  g_autofree char *text = NULL, *dur = NULL, *pos = NULL;
 
   g_assert (LIVI_IS_CONTROLS (self));
   gtk_adjustment_set_value (self->adj_duration, value);
 
-  dur = self->duration / GST_SECOND;
-  pos = value / GST_SECOND;
+  dur = cui_call_format_duration (self->duration / GST_SECOND);
+  pos = cui_call_format_duration (value / GST_SECOND);
+  text = g_strdup_printf ("%s / %s", pos, dur);
 
-  text = g_strdup_printf ("%.2" G_GUINT64_FORMAT ":%.2" G_GUINT64_FORMAT
-                          " / %.2" G_GUINT64_FORMAT " :%.2" G_GUINT64_FORMAT,
-                          pos / 60, pos % 60, dur / 60, dur % 60);
   gtk_label_set_text (self->lbl_time, text);
 }
 
